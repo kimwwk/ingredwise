e1cf36049cdb088a0faa6ede53315a73c2064fa1
Kim Wong <kimwong.wwk@gmail.com>
2023-03-25 19:06:09 -0400

    update func - ocr

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,47 @@
 const express = require('express');
 const multer = require('multer');
+const axios = require('axios');
 
 const app = express();
 
 // Set up multer to handle file uploads
 const upload = multer();
 
+// TODO: env file
+// require('dotenv').config();
+
+const API_KEY = "***REMOVED***"; // replace with your actual API key
+const ENDPOINT = '***REMOVED***';
+
+async function checkIngredients(file) {
+  try {
+    const response = await axios({
+      method: 'post',
+      url: ENDPOINT,
+      headers: {
+        'Content-Type': 'application/octet-stream',
+        'Ocp-Apim-Subscription-Key': API_KEY
+      },
+      data: file
+    });
+
+    // extract description from response data
+    console.info('response',response);
+    const result = response.json();
+    return result;
+  } catch (error) {
+    console.error(error);
+    return error;
+  }
+}
+
 // Define the /api/ingredient-check endpoint
 app.post('/api/ingredient-check', upload.single('file'), (req, res) => {
   // Handle the file upload
   const file = req.file;
   console.log(file);
+  
+  checkIngredients(file)
 
   // Send a response
   res.send('File uploaded!');
