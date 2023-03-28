aedf79934cf320396fe7f196b82d2c3f3fa4b821
sing-getproofed <sing@proofed.com>
2023-03-28 18:19:05 +0100

    update ui

--- frontend-react/src/component/Layout.jsx
+++ frontend-react/src/component/Layout.jsx
@@ -1,25 +1,31 @@
-import React from "react";
-import { Link, Outlet } from "react-router-dom";
-import { FaHome } from "react-icons/fa";
+import React from 'react';
+import { Link, Outlet } from 'react-router-dom';
+import { FaHome, FaCog } from 'react-icons/fa';
 
 const Layout = () => {
   return (
-    <div className="bg-gray-50 min-h-screen">
-      <nav className="bg-green-500">
+    <div className="bg-light-beige min-h-screen">
+      <nav className="bg-muted-coral">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex items-center justify-between h-16">
             <div className="flex items-center">
-              <Link to="/" className="flex-shrink-0 text-white">
-                <FaHome className="inline-block mr-1" />
-                <span className="font-semibold">IngredWise</span>
+              <Link to="/" className="flex-shrink-0 text-white flex items-center space-x-1">
+                <FaHome className="text-soft-teal w-6 h-6" />
+                <span className="text-xl font-semibold">IngredWise</span>
               </Link>
+            </div>
+            <div className="text-soft-teal">
+              {/* Add settings icon or other navigation elements here */}
+              <FaCog className="w-6 h-6 cursor-pointer" />
             </div>
           </div>
         </div>
       </nav>
-      <main>
-        <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
-          <Outlet />
+      <main className="bg-light-beige py-6">
+        <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+          <div className="bg-white shadow-lg rounded-lg p-6 max-w-screen-xl mx-auto overflow-auto">
+            <Outlet />
+          </div>
         </div>
       </main>
     </div>
--- frontend-react/tailwind.config.js
+++ frontend-react/tailwind.config.js
@@ -1,8 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
   theme: {
+    extend: {
+      backgroundColor: {
+        'light-beige': '#F5E9D9',
+        'muted-coral': '#FF846B',
+        'olive-green': '#8CB369',
+        'soft-teal': '#70C1B3',
+      },
+      textColor: {
+        'dark-slate-gray': '#2B2D2F',
+      },
+      borderColor: {
+        'olive-green': '#8CB369',
+      },
+    },
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
 };
+
