0d96a6ff762bd90dd7ab5b7c75429826abb06aa7
sing-getproofed <41389735+xing1106@users.noreply.github.com>
2023-04-01 17:06:07 +0100

    able to update nutrition data

--- frontend-react/src/component/NutritionContent.jsx
+++ frontend-react/src/component/NutritionContent.jsx
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 const NutritionContent = ({ nutritionResult, suggestion }) => {
   const { nutrition, per_serving_size, ingredients, additional_info } =
     nutritionResult;
+
+
+  const [nutritionData, setNutritionData] = useState(nutrition);
+
+  const handleAmountChange = (index, event) => {
+    const value = event.target.value;
+
+    // Check if the input value is a valid decimal number
+    if (/^\d*\.?\d*$/.test(value)) {
+      const updatedNutrition = [...nutritionData];
+      updatedNutrition[index].amount = value;
+      setNutritionData(updatedNutrition);
+    }
+  };
 
   return (
     <section className="flex flex-col justify-center">
@@ -11,17 +25,22 @@ const NutritionContent = ({ nutritionResult, suggestion }) => {
           <div className="rounded-md overflow-hidden shadow-md bg-white">
             <div className="px-6 py-4">
               <div className="font-bold text-gray-600 mb-2">Nutrition</div>
-              <div className="grid grid-cols-3 gap-2">
-                {nutrition.map((nutrient, index) => (
-                  <React.Fragment key={index}>
-                    <div className="text-gray-600">{nutrient.name}</div>
-                    <div className="text-gray-800 font-bold">
-                      {nutrient.amount}
-                    </div>
-                    <div className="text-gray-600">{nutrient.unit}</div>
-                  </React.Fragment>
-                ))}
-              </div>
+                <div className="grid grid-cols-3 gap-2">
+                  {nutritionData.map((nutrient, index) => (
+                    <React.Fragment key={index}>
+                      <div className="text-gray-600">{nutrient.name}</div>
+                      <div className="text-gray-800 font-bold">
+                        <input
+                          type="text"
+                          value={nutrient.amount}
+                          onChange={(event) => handleAmountChange(index, event)}
+                          className="text-center w-full"
+                        />
+                      </div>
+                      <div className="text-gray-600">{nutrient.unit}</div>
+                    </React.Fragment>
+                  ))}
+                </div>
               <div className="text-gray-600 mt-4">
                 Per Serving ({per_serving_size.amount} {per_serving_size.unit})
               </div>
